62fc4fffd2b057d7ac5cb7c248bfb6a590afb2c2
Martin_chang <Martin_chang@gmail.com>
2019-06-17 13:30:06 +0800

    init

---
+++ jc03.js
@@ -0,0 +1,23 @@
+let var1;
+document.write(var1 + "<br>");
+var1 = 123;
+document.write(typeof(var1)+ "<br>");
+
+var1 = 12.3;
+document.write(typeof(var1)+ "<br>");
+
+var1 = true;
+document.write(typeof(var1)+ "<br>");
+
+var1 = 'jc';
+document.write(typeof(var1)+ "<br>");
+
+
+const name = "JC";
+// name = 'Peter';
+
+document.write("My name is" + name + "<br>");
+document.write(`My name is ${name} <br>`);
+
+let a = 10 ,b = 3;
+document.write(`${a} + ${b} = ${a+b}`);
